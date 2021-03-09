347bb897d39f1e72b0d1f3cf87603187364e8a74
Victoria Khusti <viktorijahusti@gmail.com>
2021-03-09 14:13:59 +0200

    initial commit

---
+++ DataModeling(ASP.Net Core)/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataModeling_ASP.Net_Core_.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataModeling_ASP.Net_Core_
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Data> Datas { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ DataModeling(ASP.Net Core)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DataModeling_ASP.Net_Core_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
